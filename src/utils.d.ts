013c7a71c9eb3e71618b7e79f48c56336b4b03a3
tamuraryoya <tamuraryoya@team-lab.com>
2019-08-08 00:28:01 +0900

    🐛 Disabled ignore case

---
+++ src/utils.d.ts
@@ -0,0 +1,50 @@
+/**
+ * @file 型のユーティリティ
+ */
+
+/** オブジェクトの値を取り出す */
+export type Values<T, Default = never> = T extends { [key: string]: infer V } ? V : Default;
+
+/**
+ * Converorの引数をバリデーションする
+ * @param A Getter, Mutation, Actionのいずれかをtypeofしたもの
+ * @param P モジュールでの名前と元のメソッド名のオブジェクト
+ */
+export type Validator<A extends Record<string, any>, P> = keyof A extends never
+  ? {}
+  : keyof A extends Values<P>
+  ? Values<P> extends keyof A
+    ? Record<string, string>
+    : keyof A
+  : keyof A;
+
+/**
+ * @param A Getter, Mutation, Actionのいずれかをtypeofしたもの
+ * @param P モジュールでの名前と元のメソッド名のオブジェクト
+ */
+export type Convertor<
+  A extends Record<string, any>,
+  P extends Validator<A, P>
+> = P extends Record<string, string> ? { [K in keyof P]: A[P[K]] } : never;
+
+/**
+ * ペイロードがないプロパティのキーを取り出す
+ */
+export type PickKeyWithoutPayload<P> = P extends Record<
+  string,
+  (...args: any) => any
+>
+  ? Values<
+      { [K in keyof P]: P[K] extends (context: any) => any ? K : undefined }
+    >
+  : never;
+
+/**
+ * Omit
+ */
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+/**
+ * Nil
+ */
+export type Nil<T> = T | undefined | null;
