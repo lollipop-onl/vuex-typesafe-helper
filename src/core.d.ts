9ddbe8b32670c37e7a6975574504b06cf8dc063b
lollipop-onl <lollipop@simoochee.net>
2019-12-12 18:56:12 +0900

    🔨 Restore src dir

---
+++ src/core.d.ts
@@ -0,0 +1,46 @@
+/**
+ * @file 各種コアの定義を行う
+ */
+
+import { Store } from 'vuex';
+import {
+  State,
+  Getters,
+  Mutations,
+  Actions,
+  Commit,
+  Dispatch
+} from './base';
+import { ToNestedObject, Neverable } from './utils';
+
+/** State */
+export type DefineState<N extends string | string[], S extends State> =
+  N extends ''
+    // ルートモジュール
+    ? S
+    : N extends string
+        ? { [K in N]: S }
+        : N extends string[]
+            ? ToNestedObject<S, N>
+            : S;
+
+/** Getters */
+export type DefineGetters<G extends Getters> = Neverable<{ [K in keyof G]: ReturnType<G[K]> }>;
+
+/** Mutations */
+export type DefineMutations<M extends Mutations> = Commit<M>;
+
+/** Actions */
+export type DefineActions<A extends Actions> = Dispatch<A>;
+
+export interface DefineStoreModule<
+  NS extends string | string[],
+  S extends State,
+  G extends Getters,
+  M extends Mutations,
+  A extends Actions
+> extends Store<DefineState<NS, S>> {
+  getters: DefineGetters<G>;
+  commit: DefineMutations<M>;
+  dispatch: DefineActions<A>;
+}
---
+++ src/utils.d.ts
@@ -0,0 +1,84 @@
+/**
+ * @file 型のユーティリティ
+ */
+
+/** オブジェクトの値を取り出す */
+export type Values<T, Default = never> = T extends { [key: string]: infer V } ? V : Default;
+
+/**
+ * Converorの引数をバリデーションする
+ * @param A Getter, Mutation, Actionのいずれかをtypeofしたもの
+ * @param P モジュールでの名前と元のメソッド名のオブジェクト
+ */
+export type Validator<A extends Record<string, any>, P> = keyof A extends never
+  ? {}
+  : keyof A extends Values<P>
+  ? Values<P> extends keyof A
+    ? Record<string, string>
+    : keyof A
+  : keyof A;
+
+/**
+ * @param A Getter, Mutation, Actionのいずれかをtypeofしたもの
+ * @param P モジュールでの名前と元のメソッド名のオブジェクト
+ */
+export type Convertor<
+  A extends Record<string, any>,
+  P extends Validator<A, P>
+> = P extends Record<string, string> ? { [K in keyof P]: A[P[K]] } : never;
+
+/**
+ * ペイロードがないプロパティのキーを取り出す
+ */
+export type PickKeyWithoutPayload<P> = P extends Record<
+  string,
+  (...args: any) => any
+>
+  ? Values<
+      { [K in keyof P]: P[K] extends (context: any) => any ? K : undefined }
+    >
+  : never;
+
+/**
+ * Omit
+ */
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+/**
+ * Nil
+ */
+export type Nil<T> = T | undefined | null;
+
+/**
+ *
+ */
+export type ToObjectFromKeys<
+  V,
+  K1 extends string | undefined = undefined,
+  K2 extends string | undefined = undefined,
+  K3 extends string | undefined = undefined,
+  K4 extends string | undefined = undefined,
+  K5 extends string | undefined = undefined,
+> =
+  K1 extends undefined
+    ? V
+    : K1 extends string
+        ? { [K in K1]: ToObjectFromKeys<V, K2, K3, K4, K5> }
+        : V;
+
+/**
+ * ネストされたオブジェクトに変換する
+ */
+export type ToNestedObject<V, Ks extends string[]> = ToObjectFromKeys<V, Ks[0], Ks[1], Ks[2], Ks[3], Ks[4]>
+
+/**
+ * ２つの型が等しいかを判定する
+ */
+export type IsEquals<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+    (<T>() => T extends Y ? 1 : 2) ? true : false;
+
+/**
+ * Neverの場合にデフォルトの型を返す
+ */
+export type Neverable<T, D = {}> = IsEquals<T, never> extends true ? D : T;
